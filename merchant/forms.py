b31dda30be55f308797ceafc6c51cfd6599d776b
Haotong Chen <hereischen@gmail.com>
2019-01-12 19:28:46 +0800

    changed css
    Signed-off-by: Haotong Chen <hereischen@gmail.com>

--- merchant/forms.py
+++ merchant/forms.py
@@ -10,8 +10,7 @@ class LoginForm(forms.Form):
                                widget=forms.TextInput(
                                    attrs={"class": "form-control",
                                           "id": "id_login",
-                                          "placeholder":
-                                          "请输入登录账号"}))
+                                          "placeholder": "请输入登录账号"}))
     password = forms.CharField(label='密码', required=True, min_length=6,
                                widget=forms.PasswordInput(
                                    attrs={"class": "form-control",
@@ -26,37 +25,37 @@ class RegisterForm(forms.Form):
                                    attrs={"class": "form-control",
                                           "id": "id_username",
                                           "type": "text",
-                                          "placeholder": "请输入登录账号"}))
+                                          "placeholder": "请输入登录账号*"}))
     password = forms.CharField(initial='请输入密码', required=True, max_length=20,
                                min_length=6,
                                widget=forms.PasswordInput(
                                    attrs={"class": "form-control",
                                           "id": "id_password1",
-                                          "placeholder": "请输入密码"}))
+                                          "placeholder": "请输入密码*"}))
     password1 = forms.CharField(initial='请再输入密码', required=True, max_length=20,
                                 min_length=6,
                                 widget=forms.PasswordInput(
                                     attrs={"class": "form-control",
                                            "id": "id_password1",
-                                           "placeholder": "请确定密码"}))
-    email = forms.EmailField(required=True,
-                             widget=forms.EmailInput(
-                                 attrs={"class": "form-control",
-                                        "placeholder": "请输入邮箱"}))
+                                           "placeholder": "请确定密码*"}))
     mobile = forms.CharField(label='手机号码', required=True, max_length=11,
                              min_length=10,
                              widget=forms.TextInput(
                                  attrs={"class": "form-control",
-                                        "placeholder": "请输入手机号码"}))
+                                        "placeholder": "请输入手机号码*"}))
     name = forms.CharField(label='公司名', required=True,
                            widget=forms.TextInput(
                                attrs={"class": "form-control",
-                                      "placeholder": "请输入公司名"}))
+                                      "placeholder": "请输入公司名*"}))
     address = forms.CharField(label='公司地址', required=False,
                               widget=forms.TextInput(
                                   attrs={"class": "form-control",
                                          "placeholder": "请输入公司地址"}))
-    dingding = forms.CharField(label='公司名', required=False,
+    email = forms.EmailField(required=False,
+                             widget=forms.EmailInput(
+                                 attrs={"class": "form-control",
+                                        "placeholder": "请输入邮箱"}))
+    dingding = forms.CharField(label='钉钉', required=False,
                                widget=forms.TextInput(
                                    attrs={"class": "form-control",
                                           "placeholder": "请输入钉钉"}))
