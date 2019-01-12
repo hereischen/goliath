647afecbc6f9de2bec2bd867bf5c5cc0cb137974
Haotong Chen <hereischen@gmail.com>
2019-01-12 23:55:33 +0800

    added a SelectMerchandise
    Signed-off-by: Haotong Chen <hereischen@gmail.com>

---
+++ frontend/src/components/SelectMerchandise.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import Select from 'react-select';
+import _ from 'lodash';
+
+class SelectMerchandise extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+          selectedOption: null,
+          options: []
+        };
+        this.buildOptions = this.buildOptions.bind(this);
+        this.getMerchandise();
+    }
+
+    getMerchandise() {
+        $.get("inventory/merchandise", (data) => {
+            this.setState({
+                options: this.buildOptions(data.results)
+            });
+        });
+    }
+
+    buildOptions(merchantdise) {
+        return _.map(merchantdise, mdse => {
+            return {
+                value: mdse.id,
+                label: mdse.code
+            }
+        });
+    }
+
+    handleChange = (selectedOption) => {
+        this.setState({ selectedOption });
+        console.log(`Option selected:`, selectedOption);
+    }
+
+    render() {
+        const { selectedOption } = this.state;
+        const { options } = this.state;
+
+        return (
+            <Select
+                value={selectedOption}
+                onChange={this.handleChange}
+                options={options}
+            />
+    );
+  }
+}
+
+export default SelectMerchandise
