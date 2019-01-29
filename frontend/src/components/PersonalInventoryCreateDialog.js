7af8dbbc313eeb77a49bcf6f41959465c73fc222
= <xlli@thoughtworks.com>
2019-01-30 02:51:26 +0800

    [xiaolin] dialog confirmation

--- frontend/src/components/PersonalInventoryCreateDialog.js
+++ frontend/src/components/PersonalInventoryCreateDialog.js
@@ -13,8 +13,11 @@ class PersonalInventoryCreateDialog extends React.Component {
             selectedCategory: "",
             selectedMerchandiseId: 0,
             selectedMerchandiseCode: "",
-            price: 0,
-            quantity: 0,
+            showPriceHelpText: false,
+            showQuantityHelpText: false,
+            showMerchandiseHelpText: false,
+            price: '',
+            quantity: '',
             confirmed: false
         };
 
@@ -57,22 +60,29 @@ class PersonalInventoryCreateDialog extends React.Component {
             price: this.state.price,
             remarks: "",
         }, () => {
-            this.setState({
-                show: false,
-                confirmed: false,
-                selectedBrand: "",
-                selectedCategory: "",
-                selectedMerchandiseId: 0,
-                selectedMerchandiseCode: "",
-                price: 0,
-                quantity: 0,
-            });
+            this.resetFields();
             this.props.closeDialog();
         })
     }
 
+    resetFields() {
+        this.setState({
+            show: false,
+            confirmed: false,
+            selectedBrand: "",
+            selectedCategory: "",
+            selectedMerchandiseId: 0,
+            selectedMerchandiseCode: "",
+            showPriceHelpText: false,
+            showQuantityHelpText: false,
+            showMerchandiseHelpText: false,
+            price: '',
+            quantity: '',
+        });
+    }
+
     cancelSave() {
-        this.setState({show: false});
+        this.resetFields();
         this.props.closeDialog();
     }
 
@@ -82,27 +92,66 @@ class PersonalInventoryCreateDialog extends React.Component {
     onChangeCount(event) {
         this.setState({quantity: event.target.value});
     }
+
+    isValidQuantity() {
+        return this.state.quantity > 0;
+    }
+
+    isValidPrice() {
+        return this.state.price > 0;
+    }
+
+    isValidSelectMerchandise() {
+        return !_.isEmpty(this.state.selectedMerchandiseCode);
+    }
+
+    FormValid() {
+        const showMerchandiseHelpText = !this.isValidSelectMerchandise();
+        const showQuantityHelpText = !this.isValidQuantity();
+        const showPriceHelpText = !this.isValidPrice();
+        
+        this.setState({
+            showMerchandiseHelpText: showMerchandiseHelpText,
+            showQuantityHelpText: showQuantityHelpText,
+            showPriceHelpText: showPriceHelpText,
+        });
+
+        return !showQuantityHelpText && !showPriceHelpText && !showMerchandiseHelpText;
+    }
     getConfirmed(){
-        this.setState({confirmed: true});
+        if (this.FormValid()) {
+            this.setState({confirmed: true});
+        }
     }
     cancelConfirm(){
-        this.setState({confirmed: false});
+        this.resetFields();
     }
     getSaveDialogBody() {
-        const dialogBody = (this.state.confirmed ? 
-            (<div>请确认创建{this.state.selectedBrand}{this.state.selectedCategory}{this.state.selectedMerchandiseCode}的库存, 价格:{this.state.price}, 数量:{this.state.quantity}</div>)
-            :(<form className="fieldSection">
+        const helpText = "此项为必填项";
+        return (this.state.confirmed ?
+            (<div>请确认创建{this.state.selectedBrand}{this.state.selectedCategory}{this.state.selectedMerchandiseCode}的库存,
+                价格:{this.state.price}, 数量:{this.state.quantity}</div>)
+            : (<form className="fieldSection">
                 <div className="item">
                     <span>编码:</span>
                     <SelectMerchandise onChange={this.onChange}/>
+                    {this.state.showMerchandiseHelpText && <span style={{color: "red"}}>{helpText}</span>}
                 </div>
-                <div className="item"><span>品牌:</span><input placeholder="品牌" value={this.state.selectedBrand} disabled/></div>
-                <div className="item"><span>品类:</span><input placeholder="品类"  value={this.state.selectedCategory} disabled/></div>
-                <div className="item"><span>价格:</span><input placeholder="新建库存价格" value={this.state.price} onChange={this.onChangePrice} type="number" min="0" /></div>
-                <div className="item"><span>数量:</span><input placeholder="新建库存数量" value={this.state.quantity} onChange={this.onChangeCount} type="number" min="0" /></div>
+                <div className="item"><span>品牌:</span><input
+                    placeholder="品牌"
+                    value={this.state.selectedBrand}
+                    disabled/></div>
+                <div className="item"><span>品类:</span><input placeholder="品类" value={this.state.selectedCategory}
+                                                             disabled/></div>
+                <div className="item"><span>价格:</span>
+                    <input placeholder="新建库存价格" name="price" onChange={this.onChangePrice} type="number" min="0"/>
+                    {this.state.showPriceHelpText && <span style={{color: "red"}}>{helpText}</span>}
+                </div>
+                <div className="item"><span>数量:</span>
+                    <input placeholder="新建库存数量" onChange={this.onChangeCount} type="number" min="0"/>
+                    {this.state.showQuantityHelpText && <span style={{color: "red"}}>{helpText}</span>}
+                </div>
             </form>));
-
-        return dialogBody;
     }
 
     render() {
