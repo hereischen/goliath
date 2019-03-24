85bcf915efad4181633e3bd9a8bed174135f6572
Haotong Chen <hachen@redhat.com>
2019-03-24 19:09:44 +0800

    deprecate update_inventory
    adding: create/ deposit/ withdraw/ withdraw/others/
    
    Signed-off-by: Haotong Chen <hachen@redhat.com>

--- inventory/serializers.py
+++ inventory/serializers.py
@@ -57,27 +57,24 @@ class MerchandiseInventorySerializer(InventorySerializer):
         fields = ('id', 'price', 'quantity', 'merchant', 'modified_date')
 
 
-class UpdateInventorySerializer(serializers.Serializer):
-    """更新库存的serializer."""
+class UpdateInvtBaseSerializer(serializers.Serializer):
     current_merchant_id = serializers.CharField(required=True)
     merchandise_id = serializers.IntegerField(required=True)
     quantity = serializers.IntegerField(required=True)
-    # optional
-    price = serializers.DecimalField(required=False,
-                                     max_digits=11, decimal_places=2)
-    deposit = serializers.BooleanField(default=True)
     remarks = serializers.CharField(required=False)
-    withdraw_from = serializers.JSONField(required=False)
 
-    def create_new_inventory(self):
+
+class CreateNewInvtSerializer(UpdateInvtBaseSerializer):
+    price = serializers.DecimalField(required=True,
+                                     max_digits=11, decimal_places=2)
+
+    def create(self):
         try:
             md = Merchandise.objects.get(
                 id=self.validated_data['merchandise_id'])
             cm = Merchant.objects.get(
                 id=self.validated_data['current_merchant_id'])
         except ObjectDoesNotExist:
-            return {"result": "fail"}
-        if not self.validated_data.get("price"):
             return {"result": "fail"}
         Inventory.objects.create(merchandise=md,
                                  merchant=cm,
@@ -87,7 +84,13 @@ class UpdateInventorySerializer(serializers.Serializer):
                                  )
         return {"result": "create"}
 
-    def deposit_to_inventory(self):
+
+class DepositToInvtSerializer(UpdateInvtBaseSerializer):
+    """Merchant增自己的库存"""
+    price = serializers.DecimalField(required=False,
+                                     max_digits=11, decimal_places=2)
+
+    def deposit(self):
         try:
             inv = Inventory.objects.get(
                 merchandise=self.validated_data['merchandise_id'],
@@ -101,25 +104,61 @@ class UpdateInventorySerializer(serializers.Serializer):
         if remarks:
             inv.remarks += ("=>" + remarks)
         inv.save()
-        return {"result": "success"}
+        return {"result": "deposit success"}
 
-    def withdraw_from_inventory(self):
+
+class WithdrawFromInvtSerializer(UpdateInvtBaseSerializer):
+    """Merchant减自己的库存"""
+
+    def withdraw(self):
         try:
-            current_inv = Inventory.objects.get(
+            inv = Inventory.objects.get(
                 merchandise=self.validated_data['merchandise_id'],
                 merchant=self.validated_data['current_merchant_id'])
         except ObjectDoesNotExist:
             return {"result": "fail", "details": "找不到您的库存"}
 
-        if self.validated_data['quantity'] > current_inv.quantity:
+        if self.validated_data['quantity'] > inv.quantity:
             return {"result": "fail", "details": "您的库存不足"}
-        current_inv.quantity -= self.validated_data['quantity']
-        c_remarks = self.validated_data.get('remarks')
-        if c_remarks:
-            current_inv.remarks += ("=>" + c_remarks)
-        current_inv.save()
+        inv.quantity -= self.validated_data['quantity']
+        remarks = self.validated_data.get('remarks')
+        if remarks:
+            inv.remarks += ("=>" + remarks)
+        inv.save()
+        return {"result": "withdraw success"}
+
+
+class WithdrawFromOthersInvtSerializer(UpdateInvtBaseSerializer):
+    """Merchant借其他的库存
+
+    withdraw_from =
+    [{"merchant":"83719796-8d17-4709-a12c-c7b56b90d7fb",
+      "quantity":1
+      },
+     {
+      "merchant":"b4b33695-983d-4fea-bb51-6822f76f74c2",
+      "quantity":2,
+      "remarks":"withdraw"
+      "deal_price": 20.6
+     }
+    ]
+    """
+    withdraw_from = serializers.JSONField(required=True, default=list)
+
+    def withdraw(self):
+        try:
+            # 借库存需当前用户无库存或数量为0
+            current_inv = Inventory.objects.get(
+                merchandise=self.validated_data['merchandise_id'],
+                merchant=self.validated_data['current_merchant_id'])
+            if current_inv.quantity > 0:
+                return {"result": "fail", "details": "请先使用自己的配货"}
+        except ObjectDoesNotExist:
+            pass
 
         for item in self.validated_data.get('withdraw_from', []):
+            if item['merchant'] == self.validated_data['current_merchant_id']:
+                return {"result": "fail", "details": "错误的商户信息"}
             try:
                 withdraw_inv = Inventory.objects.get(
                     merchandise=self.validated_data['merchandise_id'],
@@ -129,8 +168,9 @@ class UpdateInventorySerializer(serializers.Serializer):
             if int(item['quantity']) > withdraw_inv.quantity:
                 return {"result": "fail", "details": "对应商户的库存不足"}
             withdraw_inv.quantity -= int(item['quantity'])
-            remarks = item.get('remarks')
-            if remarks:
-                withdraw_inv.remarks += ("=>" + remarks)
-            withdraw_inv.save()
-        return {"result": "success"}
+            # deal_price = item.get('deal_price', withdraw_inv.price)
+            # remarks = item.get('remarks')
+            # if remarks:
+            #     withdraw_inv.remarks += ("=>" + remarks)
+            # withdraw_inv.save()
+        return {"result": "withdraw success"}
