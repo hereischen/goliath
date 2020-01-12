3bf06039f9a66f5d02a5bffdd26865fbe550fb35
Haotong Chen <hereischen@gmail.com>
2020-01-12 11:26:13 +0800

    henan-2020-01-11 data
    Signed-off-by: Haotong Chen <hereischen@gmail.com>

--- scripts/load_data.py
+++ scripts/load_data.py
@@ -11,15 +11,15 @@ def load_data(xlsx):
     print('loading %s' % xlsx)
     df = pandas.read_excel(xlsx)
     for row in df.iterrows():
-        print(row[1][u'商品编码'])
-        b, _ = Brand.objects.get_or_create(brand=row[1][u'品牌'])
-        c, _ = Category.objects.get_or_create(category=row[1][u'品类'])
+        print(row[1]['C'])
+        b, _ = Brand.objects.get_or_create(brand=row[1]['A'])
+        c, _ = Category.objects.get_or_create(category=row[1]['B'])
         m, _ = Merchandise.objects.get_or_create(brand=b,
                                                  category=c,
-                                                 code=row[1][u'商品编码'],
-                                                 model=row[1][u'商品型号'],
-                                                 certification=row[1][u'认证'],
-                                                 spare_parts=row[1][u'属性'])
+                                                 code=row[1]['C'],
+                                                 model=row[1]['D'],
+                                                 certification=row[1]['E'],
+                                                 spare_parts=row[1]['F'])
 
 
-load_data('./scripts/henan-2020-01-02.xlsx')
+load_data('./scripts/henan-2020-01-11.xlsx')
